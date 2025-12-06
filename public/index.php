47a318cdd497afd0193c67715da3aa75173db99f
Mattia Pozzati <mattia.pozzati3@studio.unibo.it>
2025-12-06 20:48:54 +0100

    Definizione strumenti che ci aiuteranno a scrivere codice più moderno ed intuituvo. Ho scritto una funzione di autocaricamento dei file per utilizzare al meglio i namespace (Modules in python). Ho definito (Ancora in scrittura ) un router custom per gestire il backend come se fosse una restApi (Fast Api in python) e un modello ORM per gestire le entità del database (Non mi ricordo come si chiama la libreria pyhon).

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
+require_once "../Core/Helper/Autoloader.php";
 
-require __DIR__ . '/../src/Router.php';
-require __DIR__ . '/../src/Controller/PageController.php';
+use Core\Routing\Router;
 
 $router = new Router;
 
 // carica tutte le route
-require __DIR__ . '/../Routes/routes.php';
+require __DIR__ . '/../config/routes.php';
 
 echo "Backend in sviluppo!<br>";
 
