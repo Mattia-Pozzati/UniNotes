530b15715cf81b4aae8288de72400c6a0a262a6b
Simo <sbrunelli01@gmail.com>
2026-01-08 15:05:25 +0100

    add login/register form

--- config/routes.php
+++ config/routes.php
@@ -15,6 +15,17 @@ use Core\Routing\Router;
  */
 
 Router::getInstance()->get('/', 'PageController@index');
+
+// Auth
+Router::getInstance()->get('/login', 'AuthController@showLogin');
+Router::getInstance()->post('/login', 'AuthController@Login');
+
+Router::getInstance()->get('/register', 'AuthController@showRegister');
+Router::getInstance()->post('/register', 'AuthController@register');
+
+Router::getInstance()->get('/logout', 'AuthController@logout');
+
+// Log
 Router::getInstance()->get("/log", "LogController@show");
 
 ?>
--- public/js/DarkLightToggle.js
+++ public/js/DarkLightToggle.js
@@ -1,17 +1,40 @@
- // Theme toggle usando data-bs-theme
 const themeBtn = document.getElementById('themeToggle');
+const html = document.documentElement;
+
+function applyTheme(theme) {
+    html.setAttribute('data-bs-theme', theme);
+    if (theme === 'dark') {
+        themeBtn.innerHTML = '<i class="bi bi-sun"></i>';
+    } else {
+        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    }
+}
+
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
+
 if (themeBtn) {
     themeBtn.addEventListener('click', () => {
-        const html = document.documentElement;
-        if (html.getAttribute('data-bs-theme') === 'light') {
-            html.setAttribute('data-bs-theme', 'dark');
-            themeBtn.innerHTML = '<i class="bi bi-sun"></i>';
-        } else {
-            html.setAttribute('data-bs-theme', 'light');
-            themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
-        }
+        const currentTheme = html.getAttribute('data-bs-theme');
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        
+        applyTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
     });
 }
 
-// Login demo
-document.getElementById('loginBtn').addEventListener('click', () => alert('Login cliccato!')); 
+// if (themeBtn) {
+//     themeBtn.addEventListener('click', () => {
+//         const html = document.documentElement;
+//         if (html.getAttribute('data-bs-theme') === 'light') {
+//             html.setAttribute('data-bs-theme', 'dark');
+//             themeBtn.innerHTML = '<i class="bi bi-sun"></i>';
+//         } else {
+//             html.setAttribute('data-bs-theme', 'light');
+//             themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+//         }
+//     });
+// }
+
+// // Login demo
+// document.getElementById('loginBtn').addEventListener('click', () => alert('Login cliccato!')); 
