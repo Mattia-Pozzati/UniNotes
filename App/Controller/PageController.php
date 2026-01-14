53a522c6bfd650978e96e56484a9801db9e9e910
simo <sbrunelli01@gmail.com>
2026-01-14 19:15:59 +0100

    refactor: update PageController to use NoteService

--- App/Controller/PageController.php
+++ App/Controller/PageController.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Controller;
+
 use App\View\View;
+use App\Service\NoteService;
 
 class PageController
 {
     public function index()
     {
-        View::render('home',"page", ["title" => "Home"]);
+        $notes = NoteService::getNotesForHome();
+        
+        View::render('home', 'page', [
+            "title" => "Home",
+            "notes" => $notes
+        ]);
     }
 }
-
-?>
-
-
